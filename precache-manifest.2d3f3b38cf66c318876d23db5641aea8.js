e12efa987a3297c83cf74c14be5b6d6508febd83
Didier Sarmiento <irvingdidier.sarmiento@gmail.com>
2020-06-11 15:09:00 -0500

    Updates

---
+++ precache-manifest.2d3f3b38cf66c318876d23db5641aea8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "084ac7b794330904cca2e22da9abad85",
+    "url": "/comunication-components-react/index.html"
+  },
+  {
+    "revision": "d71b669ff8ede3ba46ee",
+    "url": "/comunication-components-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "9ad450529dbcc9050d84",
+    "url": "/comunication-components-react/static/css/main.6a03133f.chunk.css"
+  },
+  {
+    "revision": "d71b669ff8ede3ba46ee",
+    "url": "/comunication-components-react/static/js/2.9678dada.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/comunication-components-react/static/js/2.9678dada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad450529dbcc9050d84",
+    "url": "/comunication-components-react/static/js/main.d6e5ba96.chunk.js"
+  },
+  {
+    "revision": "d5398f5e333837646382",
+    "url": "/comunication-components-react/static/js/runtime-main.963fd1ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/comunication-components-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
